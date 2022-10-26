84f09d6c1e81005e996e6165cba41767fbf9dfb6
jaber1104076 <jabercuet11@gmail.com>
2022-10-26 10:57:16 +0600

    home rout setup

--- Index.js
+++ Index.js
@@ -4,6 +4,10 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('courses server is running')
+})
+
 app.listen(port, () => {
     console.log(`courses is running on port ${port}`)
 })
